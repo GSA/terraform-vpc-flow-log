246b459af1187883f5ebd234f6e3c08780429567
Aidan Feldman <aidan.feldman@gsa.gov>
2017-11-08 11:26:50 -0500

    make traffic type captured by the flow logs configurable

--- variables.tf
+++ variables.tf
@@ -4,3 +4,8 @@ variable "prefix" {
   description = "The prefix for the resource names. You will probably want to set this to the name of your VPC, if you have multiple."
   default = "vpc"
 }
+
+variable "traffic_type" {
+  default = "ALL"
+  description = "https://www.terraform.io/docs/providers/aws/r/flow_log.html#traffic_type"
+}
