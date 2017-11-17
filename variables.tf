22425d215f293029da6965ef0d3cef888e4eb795
Aidan Feldman <aidan.feldman@gsa.gov>
2017-11-17 18:13:36 -0500

    make the CloudWatch Logs group name configurable
    Before, the group name was fixed, albeit with a configurable prefix.
    This is a problem for existing systems that may want to adopt this
    module, since they might already have flow logs going to a log group
    with a different name. We want them to be able to use the same log
    group.

--- variables.tf
+++ variables.tf
@@ -9,3 +9,13 @@ variable "traffic_type" {
   default = "ALL"
   description = "https://www.terraform.io/docs/providers/aws/r/flow_log.html#traffic_type"
 }
+
+// workaround for not being able to do interpolation in variable defaults
+// https://github.com/hashicorp/terraform/issues/4084
+locals {
+  default_log_group_name = "${var.prefix}-flow-log"
+}
+variable "log_group_name" {
+  default = ""
+  description = "Defaults to `$${default_log_group_name}`"
+}
